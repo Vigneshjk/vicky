53f6c5ad6d67fdb174e670f67aea8771c302167a
vignesh <42763643+Vigneshjk@users.noreply.github.com>
2018-09-24 12:07:08 +0530

    Create Mains.java

---
+++ Mains.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Mains {
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   char ch;
+		Scanner s=new Scanner(System.in);
+		ch=s.next().charAt(0);
+    
+ if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')){
+   if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+   {
+     System.out.println(" Vowel");
+   }
+   else
+   {
+     System.out.println("Consonant");
+   }
+ }
+	 else{
+	System.out.println("invalid");	
+   }
+  
+  }
+}
