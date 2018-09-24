1dfba41fde58c0dc7312c21c363d87dbfdd34416
vignesh <42763643+Vigneshjk@users.noreply.github.com>
2018-09-24 11:07:50 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   int n;
+		Scanner s=new Scanner(System.in);
+		n= s.nextInt();
+	
+		if(n>0)
+		{
+			System.out.println("Postive");
+		}
+		
+		else if(n==0)
+		{
+			System.out.println("zero");
+		}
+    else{
+    System.out.println("Negative");
+    }
+  
+	}
+}
