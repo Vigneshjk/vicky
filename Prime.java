eab5425e7b9e670c429ff5514f579d2f80e3f43b
vignesh <42763643+Vigneshjk@users.noreply.github.com>
2018-09-24 11:29:01 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Prime {
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   int n;
+		Scanner s=new Scanner(System.in);
+		n= s.nextInt();
+	
+		if(n % 2 ==0)
+		{
+			System.out.println("Even");
+		}
+		
+		else if(n<0)
+		{
+			System.out.println("Invalid");
+		}
+    else
+    {
+    System.out.println("Odd");
+    }
+  
+	}
+}
