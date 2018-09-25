b8c711064cefdb41e10d0999e7e5b22655106a85
vignesh <42763643+Vigneshjk@users.noreply.github.com>
2018-09-25 09:07:52 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,23 @@
+class Array
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+ int n,k;
+ int sum=0;
+ int[] av;
+ Scanner s=new Scanner(System.in);
+ n=s.nextInt();
+ k=s.nextInt();
+ av=new int[n];
+ for(int i=0;i<n;i++)
+ {
+ 	av[i]=s.nextInt();
+ 
+ }
+ for(int i=0;i<k;i++)
+ {
+ 	sum=sum+av[i];
+ }
+ System.out.println(+sum);
+	}
+}
